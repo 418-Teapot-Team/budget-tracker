dbbd01647a916b3a7042ab977555277889c61f11
BoryslavGlov <boryslav.ziubrytskyi@providers.glovoapp.com>
2023-05-14 00:48:29 +0300

    feature: add route & gitignore

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type St struct {
+	Name string `json:"name"`
+}
+
+func testApi(w http.ResponseWriter, r *http.Request) {
+
+	st := St{Name: "Roman"}
+
+	b, _ := json.Marshal(st)
+
+	w.Write(b)
+	return
+
+}
 
 func main() {
-	fmt.Println("hello world")
+	http.HandleFunc("/test", testApi)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
