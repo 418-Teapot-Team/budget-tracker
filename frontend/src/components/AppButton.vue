a7fda33a6acbd9ed79da5742366a73c9803b0cff
Kolia913 <mynew1mail470@gmail.com>
2023-05-15 02:12:39 +0300

    added auth functionality

---
+++ frontend/src/components/AppButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button
+    class="border-2 border-black p-2 text-center w-full rounded-xl"
+    :class="
+      isOutline
+        ? 'transition duration-300 hover:bg-black hover:text-white text-black'
+        : 'bg-black text-white'
+    "
+    :type="type ?? type"
+    @click="onClick"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'AppButton',
+  props: {
+    text: String,
+    isOutline: Boolean,
+    type: String,
+  },
+  methods: {
+    onClick() {
+      this.$emit('click');
+    },
+  },
+};
+</script>
