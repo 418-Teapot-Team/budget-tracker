c01431b4d7c41767eff387ccc1c06d3777d7e5c1
BoryslavGlov <boryslav.ziubrytskyi@providers.glovoapp.com>
2023-05-16 22:07:21 +0300

    fix: base url change

--- backend/migrations/000001_init.up.sql
+++ backend/migrations/000001_init.up.sql
@@ -9,11 +9,11 @@ CREATE TABLE users
 create table lists
 (
     id         bigint auto_increment primary key,
-    user_id    bigint                    not null,
+    user_id    bigint                     not null,
     type       ENUM ('income','expenses') not null,
-    category   varchar(255)              not null,
-    comment    varchar(255)              null default null,
-    amount     float                     not null,
-    created_at datetime                  not null,
+    category   varchar(255)               not null,
+    comment    varchar(255)               null default null,
+    amount     float                      not null,
+    created_at datetime                   not null,
     foreign key (user_id) references users (id)
 );
