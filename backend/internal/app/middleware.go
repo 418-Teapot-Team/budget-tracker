630d7e7a1a107b257ccc6057ae0d124d0b31558f
BoryslavGlov <boryslav.ziubrytskyi@providers.glovoapp.com>
2023-05-19 01:26:10 +0300

    fix: corses

--- backend/internal/app/middleware.go
+++ backend/internal/app/middleware.go
@@ -53,12 +53,8 @@ func (app *App) getUserId(c *gin.Context) (int, error) {
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Header("Content-Type", "application/json")
 		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Credentials", "true")
 		c.Header("Access-Control-Allow-Headers", "*")
-		c.Header("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
-
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
 			return
