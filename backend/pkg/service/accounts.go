d48c7b4d71ea79d643fa0e1be4c2b8bbaf42d849
BoryslavGlov <boryslav.ziubrytskyi@providers.glovoapp.com>
2023-05-19 18:39:51 +0300

    fix: loans fix

--- backend/pkg/service/accounts.go
+++ backend/pkg/service/accounts.go
@@ -110,9 +110,6 @@ func getUpdatedStruct(finance budget.Account) (budget.Account, int) {
 	}
 
 	goalSum := mounthPayment*float64(finance.MonthAmount) + finance.Sum
-	if finance.Type == "credit" {
-		goalSum = -finance.Sum
-	}
 
 	update := budget.Account{
 		ID:              finance.ID,
